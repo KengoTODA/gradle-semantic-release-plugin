8b816260379fb56989a36bc13e77fe5cab6ab712
Kengo TODA <skypencil@gmail.com>
2019-03-19 17:46:11 +0800

    feat: implement verifyConditions step

--- src/verify-conditions.ts
+++ src/verify-conditions.ts
@@ -1,6 +1,15 @@
 import {IContext} from "./definition";
+import {getCommand, getTaskToPublish} from "./gradle";
 
 module.exports = async function verifyConditions(pluginConfig: object, context: IContext) {
-  const {logger} = context;
-  logger.info(`Hello verifyConditions! Your config is:${JSON.stringify(pluginConfig)}`);
+  const {cwd, env, logger} = context;
+  const command = await getCommand(cwd);
+  if (command !== "./gradlew") {
+    throw new Error(`Gradle wrapper not found at ${cwd}`);
+  }
+  const task = await getTaskToPublish(cwd, env as NodeJS.ProcessEnv);
+  if (task === "") {
+    throw new Error("No task found that can publish artifacts");
+  }
+  logger.debug("Verified conditions, and found no problem");
 };
