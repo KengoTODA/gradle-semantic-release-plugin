60eb7c4a460b2b9f44cee08eb60c9d2686a95442
Kengo TODA <skypencil@gmail.com>
2019-03-21 11:44:44 +0800

    fix: remove needless task for verify-release step

--- src/verify-release.ts
+++
@@ -1,6 +0,0 @@
-import {IContext} from "./definition";
-
-module.exports = async function verifyRelease(pluginConfig: object, context: IContext) {
-  const {logger} = context;
-  logger.info(`Hello verifyRelease! Your config is:${JSON.stringify(pluginConfig)}`);
-};
