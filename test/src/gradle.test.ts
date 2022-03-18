27560ab4854c996c45841a23460e7c0450aca2bb
Kengo TODA <skypencil@gmail.com>
2022-03-18 08:41:59 +0800

    test: reorganize test cases

--- test/src/gradle.test.ts
+++ test/src/gradle.test.ts
@@ -14,86 +14,95 @@ import { beforeEach, describe, expect, it, jest } from "@jest/globals";
 
 describe("Test for gradle handling", function () {
   jest.setTimeout(60000);
-  it("getCommand() return 'gradle' when there is no gradle wrapper", async () => {
-    const command = await getCommand(cwd());
-    expect(command).toBe("gradle");
-  });
-  it("getCommand() can find the wrapper script", async () => {
-    const gradleProject = join(cwd(), "test/project/without-plugin");
-    const command = await getCommand(gradleProject);
-    expect(command).toBe("./gradlew");
-  });
-
-  it("getTaskToPublish() return empty string when there is no task to publish", async () => {
-    const gradleProject = join(cwd(), "test/project/without-plugin");
-    const task = await getTaskToPublish(
-      gradleProject,
-      process.env,
-      new Signale()
-    );
-    expect(task).toBe("");
-  });
-  it("getTaskToPublish() return 'publish' when there is maven-publish-plugin", async () => {
-    const gradleProject = join(cwd(), "test/project/with-maven-publish-plugin");
-    const task = await getTaskToPublish(
-      gradleProject,
-      process.env,
-      new Signale()
-    );
-    expect(task).toBe("publish");
-  });
-  it("getTaskToPublish() return 'uploadArchives' when there is available legacy publishing method", async () => {
-    const gradleProject = join(cwd(), "test/project/with-legacy-publishing");
-    const task = await getTaskToPublish(
-      gradleProject,
-      process.env,
-      new Signale()
-    );
-    expect(task).toBe("uploadArchives");
-  });
-  it("getTaskToPublish() return 'artifactoryDeploy' when there is available artifactory-plugin", async () => {
-    const gradleProject = join(cwd(), "test/project/with-artifactory-plugin");
-    const task = await getTaskToPublish(
-      gradleProject,
-      process.env,
-      new Signale()
-    );
-    expect(task).toBe("artifactoryDeploy");
-  });
-  it("getTaskToPublish() return 'publishPlugins' when there is available plugin-publish-plugin", async () => {
-    const gradleProject = join(
-      cwd(),
-      "test/project/with-plugin-publish-plugin"
-    );
-    const task = await getTaskToPublish(
-      gradleProject,
-      process.env,
-      new Signale()
-    );
-    expect(task).toBe("publishPlugins");
-  });
-  it("getTaskToPublish() return 'publishPlugins' when there is available plugin-publish-plugin and maven-publish", async () => {
-    const gradleProject = join(
-      cwd(),
-      "test/project/with-plugin-publish-and-maven-publish"
-    );
-    const task = await getTaskToPublish(
-      gradleProject,
-      process.env,
-      new Signale()
-    );
-    expect(task).toBe("publishPlugins");
+  describe("getCommand()", () => {
+    it("returns 'gradle' when there is no gradle wrapper", async () => {
+      const command = await getCommand(cwd());
+      expect(command).toBe("gradle");
+    });
+    it("finds the wrapper script", async () => {
+      const gradleProject = join(cwd(), "test/project/without-plugin");
+      const command = await getCommand(gradleProject);
+      expect(command).toBe("./gradlew");
+    });
   });
 
-  it("getVersion() returns version defined in build.gradle", async () => {
-    const gradleProject = join(cwd(), "test/project/without-properties-file");
-    const version = await getVersion(gradleProject, process.env);
-    expect(version).toBe("1.2.3");
+  describe("getTaskToPublish()", () => {
+    it("returns empty string when there is no task to publish", async () => {
+      const gradleProject = join(cwd(), "test/project/without-plugin");
+      const task = await getTaskToPublish(
+        gradleProject,
+        process.env,
+        new Signale()
+      );
+      expect(task).toBe("");
+    });
+    it("returns 'publish' when there is maven-publish-plugin", async () => {
+      const gradleProject = join(
+        cwd(),
+        "test/project/with-maven-publish-plugin"
+      );
+      const task = await getTaskToPublish(
+        gradleProject,
+        process.env,
+        new Signale()
+      );
+      expect(task).toBe("publish");
+    });
+    it("returns 'uploadArchives' when there is available legacy publishing method", async () => {
+      const gradleProject = join(cwd(), "test/project/with-legacy-publishing");
+      const task = await getTaskToPublish(
+        gradleProject,
+        process.env,
+        new Signale()
+      );
+      expect(task).toBe("uploadArchives");
+    });
+    it("returns 'artifactoryDeploy' when there is available artifactory-plugin", async () => {
+      const gradleProject = join(cwd(), "test/project/with-artifactory-plugin");
+      const task = await getTaskToPublish(
+        gradleProject,
+        process.env,
+        new Signale()
+      );
+      expect(task).toBe("artifactoryDeploy");
+    });
+    it("returns 'publishPlugins' when there is available plugin-publish-plugin", async () => {
+      const gradleProject = join(
+        cwd(),
+        "test/project/with-plugin-publish-plugin"
+      );
+      const task = await getTaskToPublish(
+        gradleProject,
+        process.env,
+        new Signale()
+      );
+      expect(task).toBe("publishPlugins");
+    });
+    it("returns 'publishPlugins' when there is available plugin-publish-plugin and maven-publish", async () => {
+      const gradleProject = join(
+        cwd(),
+        "test/project/with-plugin-publish-and-maven-publish"
+      );
+      const task = await getTaskToPublish(
+        gradleProject,
+        process.env,
+        new Signale()
+      );
+      expect(task).toBe("publishPlugins");
+    });
   });
-  it("getVersion() returns version defined in gradle.properties", async () => {
-    const gradleProject = join(cwd(), "test/project/with-properties-file");
-    const version = await getVersion(gradleProject, process.env);
-    expect(version).toBe("0.1.2");
+
+  describe("getVersion()", () => {
+    it("returns version defined in build.gradle", async () => {
+      const gradleProject = join(cwd(), "test/project/without-properties-file");
+      const version = await getVersion(gradleProject, process.env);
+      expect(version).toBe("1.2.3");
+    });
+    it("returns version defined in gradle.properties", async () => {
+      const gradleProject = join(cwd(), "test/project/with-properties-file");
+      const version = await getVersion(gradleProject, process.env);
+      expect(version).toBe("0.1.2");
+    });
   });
 
   describe("publishArtifact()", () => {
